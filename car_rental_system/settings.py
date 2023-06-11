6562cfbb9e7fa9a16794bec93695d1a023506707
kobi <kobiokiti@gmail.com>
2023-06-11 22:33:17 +0100

    fixeing error 500

--- car_rental_system/settings.py
+++ car_rental_system/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r#^jvqmay8_v*_yb=f6@55(_g^=!_mz_r)&n0pe52lw-ly7*a3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
